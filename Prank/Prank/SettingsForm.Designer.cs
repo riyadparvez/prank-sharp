4e093a0850190dc9eb55c5761d557479fcacb4e4
Riyad Parvez <social.riyad@gmail.com>
2013-12-08 00:14:48 +0600

    added settings form

--- Prank/Prank/SettingsForm.Designer.cs
+++ Prank/Prank/SettingsForm.Designer.cs
@@ -28,23 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.okCancelPanel = new System.Windows.Forms.Panel();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.okCancelPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // okCancelPanel
+            // 
+            this.okCancelPanel.BackColor = System.Drawing.Color.Transparent;
+            this.okCancelPanel.Controls.Add(this.cancelButton);
+            this.okCancelPanel.Controls.Add(this.okButton);
+            this.okCancelPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.okCancelPanel.Location = new System.Drawing.Point(0, 215);
+            this.okCancelPanel.Name = "okCancelPanel";
+            this.okCancelPanel.Size = new System.Drawing.Size(284, 47);
+            this.okCancelPanel.TabIndex = 0;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(45, 12);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(153, 11);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
             // 
             // SettingsForm
             // 
+            this.AcceptButton = this.okButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.okCancelPanel);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SettingsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Settings";
+            this.okCancelPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel okCancelPanel;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button okButton;
     }
 }
