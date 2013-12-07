57c7c35d627e56b8027cc5c258e03fa566f2116b
Riyad Parvez <social.riyad@gmail.com>
2013-12-07 21:55:06 +0600

    added config file

--- Prank/Prank/Program.cs
+++ Prank/Prank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -8,6 +9,13 @@ namespace Prank
 {
     static class Program
     {
+        public static PrankConfigurationSection Config { get; internal set; }
+
+        public static void Initialize()
+        {
+            Config = ConfigurationManager.GetSection("prank") as PrankConfigurationSection;
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
